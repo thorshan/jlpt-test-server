1069b08474bfaa5f57b1991bbef02c3946b66bc3
thorshan <myothunaing18@gmail.com>
2026-03-10 11:33:41 +0630

    GG

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,12 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import mongoose from "mongoose";
-import app from "./app.js";
+import { app } from "./app.js";
 
-const PORT = process.env.PORT ? parseInt(process.env.PORT) : 5000;
-const MONGO_URI = process.env.DB_URI;
+const PORT = process.env.PORT || 3000;
 
-if (!MONGO_URI) {
-  throw new Error("MONGO_URI environment variable is not defined");
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`[SYSTEM] : Local Server running on port ${PORT}`);
+  });
 }
-
-(async () => {
-  try {
-    await mongoose.connect(MONGO_URI);
-    console.log("[MONGODB] : Database connected");
-
-    app.listen(PORT, () => {
-      console.log(`[SYSTEM] : Server running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error("[SYSTEM] : Failed to connect to MongoDB", error);
-    process.exit(1);
-  }
-})();
