af0e874066c32bff5127d71ba564b10fd0c166f5
thorshan <myothunaing18@gmail.com>
2026-04-08 14:47:10 +0630

    FIX

--- src/models/Activity.ts
+++ src/models/Activity.ts
@@ -9,7 +9,11 @@ const ActivitySchema = new mongoose.Schema({
   user: { type: String, default: "System" },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   status: { type: String, enum: ["SUCCESS", "FAILED"], default: "SUCCESS" },
-  createdAt: { type: Date, default: Date.now },
+  createdAt: { 
+    type: Date, 
+    default: Date.now,
+    expires: 30 * 24 * 60 * 60 // Auto-delete after 1 month
+  },
 });
 
 export default mongoose.model("Activity", ActivitySchema);
